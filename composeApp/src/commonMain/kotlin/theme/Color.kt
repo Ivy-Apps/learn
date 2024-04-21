69180006ae78801a518fc6d0de35e26e220a7578
Nicole <nicole.georgievva@gmail.com>
2024-04-21 22:37:26 +0300

    Create Material theme

--- composeApp/src/commonMain/kotlin/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/theme/Color.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,9 +25,19 @@ val Blue = ColorShades(
     dark = Color(0xFF008AE0)
 )
 
+val BlueVariant = ColorShades(
+    light = Color(0xFF0B7AE8),
+    dark = Color(0xFF0B7AE8)
+)
+
 val Orange = ColorShades(
     light = Color(0xFFF59331),
     dark = Color(0xFFF59331)
+)
+
+val OrangeVariant = ColorShades(
+    light = Color(0xFFE87A0B),
+    dark = Color(0xFFE87A0B)
 )
 
 val Red = ColorShades(
@@ -45,10 +56,17 @@ val Gray = ColorShades(
 )
 
 @Composable
+fun ColorShades.themed(): Color {
+    return if (MaterialTheme.colors.isLight) light else dark
+}
+
+@Composable
 fun ColorsDemo() {
     LazyColumn {
         colorItem(name = "Blue", colorShades = Blue)
+        colorItem(name = "Blue Variant", colorShades = BlueVariant)
         colorItem(name = "Orange", colorShades = Orange)
+        colorItem(name = "Orange Variant", colorShades = OrangeVariant)
         colorItem(name = "Red", colorShades = Red)
         colorItem(name = "Green", colorShades = Green)
         colorItem(name = "Gray", colorShades = Gray)
--- composeApp/src/commonMain/kotlin/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/theme/Theme.kt
@@ -1,47 +1,54 @@
 package theme
 
-//private val LightColorScheme = lightColors(
-//    primary = Purple.light,
-//    primaryVariant =,
-//    secondary =,
-//    secondaryVariant =,
-//    background =,
-//    surface =,
-//    error =,
-//    onPrimary =,
-//    onSecondary =,
-//    onBackground =,
-//    onSurface =,
-//    onError =
-//)
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-//private val DarkColorScheme = darkColors(
-//    primary =,
-//    primaryVariant =,
-//    secondary =,
-//    secondaryVariant =,
-//    background =,
-//    surface =,
-//    error =,
-//    onPrimary =,
-//    onSecondary =,
-//    onBackground =,
-//    onSurface =,
-//    onError =
-//)
-//
-//@Composable
-//fun LearnTheme(
-//    darkTheme: Boolean = isSystemInDarkTheme(),
-//    content: @Composable () -> Unit
-//) {
-//    val colorScheme = when(darkTheme) {
-//        true -> DarkColorScheme
-//        false -> LightColorScheme
-//    }
-//
-//    MaterialTheme(
-//        colors = colorScheme,
-//        content = content
-//    )
-//}
+private val LightColorScheme = lightColors(
+    primary = Blue.light,
+    primaryVariant = BlueVariant.light,
+    secondary = Orange.light,
+    secondaryVariant = OrangeVariant.light,
+    background = Color.White,
+    surface = Color.White,
+    error = Red.light,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    onError = Color.White
+)
+
+private val DarkColorScheme = darkColors(
+    primary = Blue.dark,
+    primaryVariant = BlueVariant.dark,
+    secondary = Orange.dark,
+    secondaryVariant = OrangeVariant.dark,
+    background = Color.Black,
+    surface = Color.Black,
+    error = Red.dark,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    onError = Color.White
+)
+
+@Composable
+fun LearnTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when (darkTheme) {
+        true -> DarkColorScheme
+        false -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colors = colorScheme,
+        content = content
+    )
+}
