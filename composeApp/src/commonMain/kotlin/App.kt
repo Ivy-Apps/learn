093771c887f263ac264ff2273d5273ba6f11a441
Nicole Georgieva <93789076+nicolegeorgieva@users.noreply.github.com>
2024-04-21 22:42:26 +0300

    Theme and components (#1)
    * Add colors
    
    * Create Material theme

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -5,15 +5,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import learn.composeapp.generated.resources.Res
+import learn.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import learn.composeapp.generated.resources.Res
-import learn.composeapp.generated.resources.compose_multiplatform
+import theme.ColorsDemo
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -22,6 +26,7 @@ fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            ColorsDemo()
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
