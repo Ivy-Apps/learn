1ae0a2cf70d582947368eb84bd8dee54b948b47b
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-22 16:28:24 +0300

    Fix JS not supported APIs

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,17 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import ivy.di.AppDiModule
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import theme.ColorsDemo
 
 @Composable
 @Preview
 fun App() {
+    LaunchedEffect(Unit) {
+        AppDiModule.init()
+    }
+
     MaterialTheme {
         ColorsDemo()
     }
--- shared/src/commonMain/kotlin/ivy/di/DiContainer.kt
+++ shared/src/commonMain/kotlin/ivy/di/DiContainer.kt
@@ -1,5 +1,7 @@
 package ivy.di
 
+import kotlin.reflect.KClass
+
 
 object Di {
 
@@ -16,20 +18,20 @@ object Di {
     }
 
     inline fun <reified T : Any> DiScope.register(noinline factory: () -> T) {
-        factories[DependencyKey(this, T::class.qualifiedName!!)] = factory
+        factories[DependencyKey(this, T::class.diGraphKey())] = factory
     }
 
     inline fun <reified T : Any> DiScope.singleton(noinline factory: () -> T) {
-        val className = T::class.qualifiedName!!
+        val className = T::class.diGraphKey()
         factories[DependencyKey(this, className)] = factory
         singletons.add(className)
     }
 
     inline fun <reified T : Any> get(): T {
-        val className = T::class.qualifiedName!!
-        val (scope, factory) = factory(className)
-        val depKey = DependencyKey(scope, className)
-        return if (className in singletons) {
+        val classKey = T::class.diGraphKey()
+        val (scope, factory) = factory(classKey)
+        val depKey = DependencyKey(scope, classKey)
+        return if (classKey in singletons) {
             if (depKey in instances) {
                 // single instance already created
                 instances[depKey] as T
@@ -57,9 +59,14 @@ object Di {
 
     inline fun scopedFactory(
         scope: DiScope,
-        className: String
-    ): Pair<DiScope, () -> Any>? = factories[DependencyKey(scope, className)]?.let {
+        classKey: String
+    ): Pair<DiScope, () -> Any>? = factories[DependencyKey(scope, classKey)]?.let {
         scope to it
+    }
+
+    inline fun <reified T : Any> KClass<T>.diGraphKey(): String {
+        // TODO: type.qualifiedName doesn't work because it's not supported on JS
+        return simpleName!!
     }
 
     fun clear(scope: DiScope) {
