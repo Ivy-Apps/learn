302393b2a1b4aafe2b2c7a4ccb6ff7e10972e515
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-22 23:54:47 +0300

    Remove unnecessary configuration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,62 +1,7 @@
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
-
 plugins {
     id("ivy.shared-module")
     id("ivy.serialization")
     id("ivy.ktor-client")
-}
-
-kotlin {
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser {
-            commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(project.projectDir.path)
-                    }
-                }
-            }
-        }
-    }
-
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
-
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    jvm()
-
-    sourceSets {
-        commonMain.dependencies {
-            implementation(libs.kotlin.serialization)
-            implementation(libs.bundles.ktor.client.common)
-        }
-        androidMain.dependencies {
-            implementation(libs.ktor.client.android)
-        }
-        iosMain.dependencies {
-            implementation(libs.ktor.client.darwin)
-        }
-        named("wasmJsMain").dependencies {
-            implementation(libs.ktor.client.js)
-        }
-        jvmMain.dependencies {
-            implementation(libs.ktor.client.java)
-        }
-        jvmTest.dependencies {
-            implementation(libs.bundles.test)
-        }
-    }
 }
 
 android {
