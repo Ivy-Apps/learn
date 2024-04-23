f54b683cf056954b009312b1e43aa2fdafc04eb5
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-23 18:18:37 +0300

    Fix Web on mobile #2

--- composeApp/src/jsMain/kotlin/main.js.kt
+++ composeApp/src/jsMain/kotlin/main.js.kt
@@ -1,10 +1,17 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.CanvasBasedWindow
 import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     onWasmReady {
-        CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
+        CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+            Box(Modifier.fillMaxSize()) {
+                App()
+            }
+        }
     }
 }
