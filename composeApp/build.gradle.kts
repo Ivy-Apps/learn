00148b1487c7e92d4f6c486f65a774bae5eec503
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-23 15:50:21 +0300

    Cleanup dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -62,6 +62,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.kotlin.immutableCollections)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
