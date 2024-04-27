5fb0e124642b4823e4f4837030e590bb8ff786b7
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-27 19:48:20 +0300

    Add workflow to run BE tests

--- server/src/main/kotlin/ivy/learn/Application.kt
+++ server/src/main/kotlin/ivy/learn/Application.kt
@@ -1,36 +1,20 @@
 package ivy.learn
 
-import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import ivy.di.Di
+import ivy.di.SharedModule
 import ivy.learn.data.di.DataModule
 import ivy.learn.di.AppModule
 
 fun main() {
-    Di.init(modules = setOf(DataModule, AppModule))
-
+    Di.init(modules = setOf(SharedModule, DataModule, AppModule))
     val app = Di.get<LearnApp>()
 
     embeddedServer(
         Netty,
         port = System.getenv("PORT")?.toInt() ?: 8080,
         host = "0.0.0.0",
-        module = {
-            app.init()
-        }
+        module = { app.init(this) },
     ).start(wait = true)
-}
-
-fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText("Ktor: Hello from BE!")
-        }
-        get("/version") {
-            call.respondText("1.0.0")
-        }
-    }
 }
--- server/src/main/kotlin/ivy/learn/LearnApp.kt
+++ server/src/main/kotlin/ivy/learn/LearnApp.kt
@@ -3,6 +3,7 @@ package ivy.learn
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import ivy.di.Di
+import ivy.di.Di.register
 import ivy.learn.api.AnalyticsApi
 import ivy.learn.api.Api
 import ivy.learn.data.database.ExposedDatabase
@@ -10,17 +11,26 @@ import ivy.learn.data.database.ExposedDatabase
 class LearnApp(
     private val database: ExposedDatabase
 ) {
-    private val apis = setOf<Api>(
-        Di.get<AnalyticsApi>()
-    )
+    private val apis by lazy {
+        setOf<Api>(
+            Di.get<AnalyticsApi>()
+        )
+    }
 
-    fun Application.init() {
+    private fun onDi() = Di.appScope {
+        register { AnalyticsApi() }
+    }
+
+    fun init(ktorApp: Application) {
         database.init().onLeft {
             throw InitializationError("Failed to initialize database: $it")
         }
+        onDi()
 
-        routing {
-            apis.forEach { it.endpoints() }
+        ktorApp.routing {
+            apis.forEach { api ->
+                with(api) { endpoints() }
+            }
         }
     }
 }
