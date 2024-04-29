e113fe51340c64a117ed42a516a01bb8c7470042
Iliyan Germanov <iliyan.germanov971@gmail.com>
2024-04-29 14:59:11 +0300

    Setup Lottie animations (#13)
    * Add support for Lottie animations
    
    * Make a nice Lottie API

--- composeApp/src/commonMain/kotlin/ui/screen/intro/composable/IntroContent.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/intro/composable/IntroContent.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import component.LearnLottieAnimation
 import ui.screen.intro.IntroViewEvent
 import ui.screen.intro.IntroViewState
 
@@ -16,17 +18,23 @@ fun IntroContent(
     viewState: IntroViewState,
     onEvent: (IntroViewEvent) -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Hello!")
-        Text(text = "Learn programming by thinking.")
-        Button(onClick = {
-            onEvent(IntroViewEvent.OnContinueClick)
-        }) {
-            Text("Let's go")
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            LearnLottieAnimation(
+                animationFile = "intro_lottie_anim.json",
+                repeat = true
+            )
+            Text(text = "Hello!")
+            Text(text = "Learn programming by thinking.")
+            Button(onClick = {
+                onEvent(IntroViewEvent.OnContinueClick)
+            }) {
+                Text("Let's go")
+            }
         }
     }
 }
