2192c7647d277fa09bd7f4ca60ad087a789d77a0
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-30 21:22:37 +0300

    Cleanup Jitpack.io stuff

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,19 +2,6 @@ plugins {
     id("ivy.shared-module")
     id("ivy.serialization")
     id("ivy.ktor-client")
-    `maven-publish`
-}
-
-group = "ivy.learn.shared"
-version = "1.0.6"
-
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
-        }
-    }
 }
 
 android {
