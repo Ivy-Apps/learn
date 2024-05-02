ddd28bb501fe34d220fd7d5fd72d6b7dc4a9215f
iliyangermanov <iliyan.germanov971@gmail.com>
2024-05-03 00:55:18 +0300

    Refactor LessonsApi

--- server/src/main/kotlin/ivy/learn/api/LessonsApi.kt
+++ server/src/main/kotlin/ivy/learn/api/LessonsApi.kt
@@ -2,6 +2,7 @@ package ivy.learn.api
 
 import arrow.core.raise.ensureNotNull
 import io.ktor.server.routing.*
+import io.ktor.util.*
 import ivy.learn.api.common.Api
 import ivy.learn.api.common.endpoint
 import ivy.learn.api.common.model.ServerError
@@ -13,7 +14,11 @@ class LessonsApi(
     private val repository: LessonsRepository
 ) : Api {
     override fun Routing.endpoints() {
-        // Endpoint that gets a lesson by ID
+        lessonById()
+    }
+
+    @KtorDsl
+    private fun Routing.lessonById() {
         get("/lessons/{id}", endpoint<Lesson> { params ->
             val lessonId = params["id"]?.let(::LessonId)
             ensureNotNull(lessonId) { ServerError.BadRequest("Lesson id is missing!") }
