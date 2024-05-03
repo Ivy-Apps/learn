81b55ba6e371f2447c720bfea778cb9eee4f18bb
Nicole <nicole.georgievva@gmail.com>
2024-05-03 15:34:29 +0300

    Create HomeViewState; create HomeItemViewState

--- composeApp/src/commonMain/kotlin/ui/screen/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package ui.screen.home
 
 import androidx.compose.runtime.Composable
+import kotlinx.collections.immutable.persistentListOf
 import ui.ComposeViewModel
 import ui.navigation.Navigation
 import ui.screen.debug.ColorDemoScreen
@@ -10,7 +11,28 @@ class HomeViewModel(
 ) : ComposeViewModel<HomeViewState, HomeViewEvent> {
     @Composable
     override fun viewState(): HomeViewState {
-        return HomeViewState()
+        return HomeViewState(
+            items = persistentListOf(
+                HomeItemViewState.Section(title = "Android"),
+                HomeItemViewState.Course(
+                    id = "123",
+                    imageUrl = "https://media.istockphoto.com/id/1483105195/photo/" +
+                            "industrial-style-of-dark-interior-design-3d-render.jpg?s=1024x1024&" +
+                            "w=is&k=20&c=Go89XVVSsi53IH_nPZ2yMRLXhrz051zKh379-RrBAkQ=",
+                    name = "Kotlin fundamentals",
+                    tagLine = "Learn the advanced concepts in Kotlin"
+                ),
+                HomeItemViewState.Section(title = "Programming"),
+                HomeItemViewState.Course(
+                    id = "234",
+                    imageUrl = "https://cdn.pixabay.com/photo/2023/03/03/14/16/light-7827771_128" +
+                            "0.jpg",
+                    name = "General programming",
+                    tagLine = "Learn programming by thinking. Get in depth knowledge of how " +
+                            "complicated systems work."
+                )
+            )
+        )
     }
 
     override fun onEvent(event: HomeViewEvent) {
--- composeApp/src/commonMain/kotlin/ui/screen/home/HomeViewState.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/home/HomeViewState.kt
@@ -1,6 +1,23 @@
 package ui.screen.home
 
-class HomeViewState
+import androidx.compose.runtime.Immutable
+import kotlinx.collections.immutable.ImmutableList
+
+@Immutable
+data class HomeViewState(
+    val items: ImmutableList<HomeItemViewState>
+)
+
+@Immutable
+sealed interface HomeItemViewState {
+    data class Section(val title: String) : HomeItemViewState
+    data class Course(
+        val id: String,
+        val imageUrl: String,
+        val name: String,
+        val tagLine: String
+    ) : HomeItemViewState
+}
 
 sealed interface HomeViewEvent {
     data object OnBackClick : HomeViewEvent
