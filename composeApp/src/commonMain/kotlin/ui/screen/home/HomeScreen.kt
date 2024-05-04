901acdc2a61bb512c2324a1b7d939792d92f4408
iliyangermanov <iliyan.germanov971@gmail.com>
2024-05-04 19:12:19 +0300

    Fix CORS on the server

--- composeApp/src/commonMain/kotlin/ui/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/home/HomeScreen.kt
@@ -5,11 +5,13 @@ import ivy.di.Di
 import ivy.di.Di.register
 import ui.navigation.Screen
 import ui.screen.home.composable.HomeContent
+import ui.screen.home.mapper.HomeViewStateMapper
 
 class HomeScreen : Screen() {
     override val path: String = "home"
 
     override fun onDi(): Di.ScreenScope.() -> Unit = {
+        register { HomeViewStateMapper() }
         register { HomeViewModel(Di.get(), Di.get(), Di.get()) }
     }
 
