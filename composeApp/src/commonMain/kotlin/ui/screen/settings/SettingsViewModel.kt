c0859f4eaec7e06bd016000671d84b9ea42f89a7
Nicole <nicole.georgievva@gmail.com>
2024-11-26 23:43:59 +0200

    WIP: Implement SettingsContent

--- composeApp/src/commonMain/kotlin/ui/screen/settings/SettingsViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/settings/SettingsViewModel.kt
@@ -7,19 +7,35 @@ import ui.navigation.Navigation
 class SettingsViewModel(
     private val navigation: Navigation,
 ) : ComposeViewModel<SettingsViewState, SettingsViewEvent> {
+    private var soundsEnabled by mutableStateOf(true)
 
     @Composable
     override fun viewState(): SettingsViewState {
-        return SettingsViewState()
+        LaunchedEffect(Unit) {
+            // TODO - fetch soundsEnabled from dataStore and update state
+        }
+        return SettingsViewState(
+            soundsEnabled = getSoundsEnabled()
+        )
+    }
+
+    @Composable
+    private fun getSoundsEnabled(): Boolean {
+        return soundsEnabled
     }
 
     override fun onEvent(event: SettingsViewEvent) {
         when (event) {
             SettingsViewEvent.OnBackClick -> handleBackClick()
+            is SettingsViewEvent.OnSoundsEnabledChange -> handleSoundsEnabledChange(event)
         }
     }
 
     private fun handleBackClick() {
         navigation.back()
+    }
+
+    private fun handleSoundsEnabledChange(event: SettingsViewEvent.OnSoundsEnabledChange) {
+        soundsEnabled = event.enabled
     }
 }
