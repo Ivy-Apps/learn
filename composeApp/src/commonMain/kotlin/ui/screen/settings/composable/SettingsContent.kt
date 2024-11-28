ab21e773fc567e63e8dc8df7eadc693a16e5e9d2
Nicole <nicole.georgievva@gmail.com>
2024-11-28 22:09:20 +0200

    Improve SettingsContent alignment

--- composeApp/src/commonMain/kotlin/ui/screen/settings/composable/SettingsContent.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/settings/composable/SettingsContent.kt
@@ -13,6 +13,7 @@ import component.BackButton
 import component.LearnScaffold
 import component.ScreenType.*
 import component.button.PrimaryButton
+import component.platformHorizontalPadding
 import component.screenType
 import component.text.Title
 import ui.screen.settings.SettingsViewEvent
@@ -30,91 +31,92 @@ fun SettingsContent(
         }),
         title = "Settings"
     ) { contentPadding ->
-        val horizontalPadding = when (screenType()) {
-            Mobile -> 8.dp
-            Tablet -> 16.dp
-            Desktop -> 16.dp
-        }
-        LazyColumn(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(contentPadding),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            contentPadding = PaddingValues(
-                top = 32.dp,
-                bottom = 48.dp,
-                start = horizontalPadding,
-                end = horizontalPadding,
-            )
+            contentAlignment = Alignment.TopCenter
         ) {
-            item(key = "premium") {
-                PrimaryButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Premium",
-                    onClick = {
-                        onEvent(SettingsViewEvent.OnPremiumClick)
-                    }
+            val horizontalPadding = platformHorizontalPadding()
+            LazyColumn(
+                modifier = Modifier.widthIn(max = 800.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                contentPadding = PaddingValues(
+                    top = 32.dp,
+                    bottom = 48.dp,
+                    start = horizontalPadding,
+                    end = horizontalPadding,
                 )
-                Spacer(Modifier.height(16.dp))
-            }
-            item(key = "app") {
-                Title("App")
-                Spacer(Modifier.height(12.dp))
-                NeutralButton(
-                    modifier = Modifier.background(color = MaterialTheme.colorsExt.backgroundVariant),
-                    onClick = {
-                        onEvent(SettingsViewEvent.OnSoundsEnabledChange(!viewState.soundsEnabled))
-                    },
-                    content = {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Text("Sounds")
-                            Spacer(Modifier.weight(1f))
-                            Switch(
-                                checked = viewState.soundsEnabled,
-                                onCheckedChange = {
-                                    onEvent(SettingsViewEvent.OnSoundsEnabledChange(it))
-                                },
-                            )
+            ) {
+                item(key = "premium") {
+                    PrimaryButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Premium",
+                        onClick = {
+                            onEvent(SettingsViewEvent.OnPremiumClick)
                         }
-                    }
-                )
-            }
-            item(key = "privacy") {
-                PrimaryButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Privacy",
-                    onClick = {
-                        onEvent(SettingsViewEvent.OnPrivacyClick)
-                    }
-                )
-                Spacer(Modifier.height(16.dp))
-            }
-            item(key = "delete-account") {
-                DeleteAccountButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Delete account",
-                    onClick = {
-                        onEvent(SettingsViewEvent.OnDeleteAccountClick)
-                    }
-                )
-                Spacer(Modifier.height(16.dp))
-            }
-            item(key = "terms-privacy") {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Text(
-                        modifier = Modifier.clickable {
-                            onEvent(SettingsViewEvent.OnTermsOfUseClick)
-                        },
-                        text = "Terms of use",
                     )
-                    Spacer(Modifier.weight(1f))
-                    Text(
-                        modifier = Modifier.clickable {
-                            onEvent(SettingsViewEvent.OnPrivacyPolicyClick)
+                    Spacer(Modifier.height(16.dp))
+                }
+                item(key = "app") {
+                    Title("App")
+                    Spacer(Modifier.height(12.dp))
+                    NeutralButton(
+                        modifier = Modifier.background(color = MaterialTheme.colorsExt.backgroundVariant),
+                        onClick = {
+                            onEvent(SettingsViewEvent.OnSoundsEnabledChange(!viewState.soundsEnabled))
                         },
-                        text = "Privacy policy",
+                        content = {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Text("Sounds")
+                                Spacer(Modifier.weight(1f))
+                                Switch(
+                                    checked = viewState.soundsEnabled,
+                                    onCheckedChange = {
+                                        onEvent(SettingsViewEvent.OnSoundsEnabledChange(it))
+                                    },
+                                )
+                            }
+                        }
                     )
+                }
+                item(key = "privacy") {
+                    PrimaryButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Privacy",
+                        onClick = {
+                            onEvent(SettingsViewEvent.OnPrivacyClick)
+                        }
+                    )
+                    Spacer(Modifier.height(16.dp))
+                }
+                item(key = "delete-account") {
+                    DeleteAccountButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Delete account",
+                        onClick = {
+                            onEvent(SettingsViewEvent.OnDeleteAccountClick)
+                        }
+                    )
+                    Spacer(Modifier.height(16.dp))
+                }
+                item(key = "terms-privacy") {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            modifier = Modifier.clickable {
+                                onEvent(SettingsViewEvent.OnTermsOfUseClick)
+                            },
+                            text = "Terms of use",
+                        )
+                        Spacer(Modifier.weight(1f))
+                        Text(
+                            modifier = Modifier.clickable {
+                                onEvent(SettingsViewEvent.OnPrivacyPolicyClick)
+                            },
+                            text = "Privacy policy",
+                        )
+                    }
                 }
             }
         }
