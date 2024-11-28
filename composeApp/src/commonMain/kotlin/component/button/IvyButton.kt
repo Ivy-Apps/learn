900ce7c44f8addf9c246db561761b149e4778d29
Nicole <nicole.georgievva@gmail.com>
2024-11-28 21:21:49 +0200

    WIP: IvyButton

--- composeApp/src/commonMain/kotlin/component/button/IvyButton.kt
+++ composeApp/src/commonMain/kotlin/component/button/IvyButton.kt
@@ -1,7 +1,8 @@
 package component.button
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -9,9 +10,9 @@ import androidx.compose.ui.unit.dp
 sealed interface ButtonAppearance {
     val style: ButtonStyle
 
-    data class Filled(override val style: ButtonStyle): ButtonAppearance
-    data class Outlined(override val style: ButtonStyle): ButtonAppearance
-    data class Text(override val style: ButtonStyle): ButtonAppearance
+    data class Filled(override val style: ButtonStyle) : ButtonAppearance
+    data class Outlined(override val style: ButtonStyle) : ButtonAppearance
+    data class Text(override val style: ButtonStyle) : ButtonAppearance
 }
 
 enum class ButtonStyle {
@@ -30,12 +31,98 @@ fun IvyButton(
     iconRight: @Composable (() -> Unit)? = null,
     onClick: () -> Unit,
 ) {
-    Button(
+    ButtonWrapper(
+        appearance = appearance,
         modifier = modifier,
         enabled = enabled,
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         onClick = onClick
     ) {
-        // TODO
+        when {
+            icon != null && text != null -> {
+
+            }
+
+            text != null && iconRight != null -> {
+
+            }
+
+            icon != null -> {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonWrapper(
+    appearance: ButtonAppearance,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    content: @Composable() (RowScope.() -> Unit)
+) {
+    val contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    val colors = appearance.buttonColors()
+
+    when (appearance) {
+        is ButtonAppearance.Filled -> {
+            Button(
+                modifier = modifier,
+                enabled = enabled,
+                contentPadding = contentPadding,
+                colors = colors,
+                onClick = onClick,
+                content = content
+            )
+        }
+
+        is ButtonAppearance.Outlined -> {
+            OutlinedButton(
+                modifier = modifier,
+                enabled = enabled,
+                contentPadding = contentPadding,
+                colors = colors,
+                onClick = onClick,
+                content = content
+            )
+        }
+        is ButtonAppearance.Text -> {
+            TextButton(
+                modifier = modifier,
+                enabled = enabled,
+                contentPadding = contentPadding,
+                colors = colors,
+                onClick = onClick,
+                content = content
+            )
+        }
+    }
+}
+
+fun ButtonAppearance.buttonColors(): ButtonColors {
+    return when (this) {
+        is ButtonAppearance.Filled -> {
+            when (this.style) {
+                ButtonStyle.Primary -> ButtonDefaults.buttonColors()
+                ButtonStyle.Secondary -> ButtonDefaults.buttonColors()
+                ButtonStyle.Neutral -> ButtonDefaults.buttonColors()
+            }
+        }
+
+        is ButtonAppearance.Outlined -> {
+            when (this.style) {
+                ButtonStyle.Primary -> ButtonDefaults.outlinedButtonColors()
+                ButtonStyle.Secondary -> ButtonDefaults.outlinedButtonColors()
+                ButtonStyle.Neutral -> ButtonDefaults.outlinedButtonColors()
+            }
+        }
+
+        is ButtonAppearance.Text -> {
+            when (this.style) {
+                ButtonStyle.Primary -> ButtonDefaults.textButtonColors()
+                ButtonStyle.Secondary -> ButtonDefaults.textButtonColors()
+                ButtonStyle.Neutral -> ButtonDefaults.textButtonColors()
+            }
+        }
     }
 }
