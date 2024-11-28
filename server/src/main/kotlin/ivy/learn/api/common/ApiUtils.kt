2f3fe66b07c0d664f92c70d6c0e5bef812357f0a
iliyangermanov <iliyan.germanov971@gmail.com>
2024-11-28 14:01:02 +0200

    Improve API base

--- server/src/main/kotlin/ivy/learn/api/common/ApiUtils.kt
+++ server/src/main/kotlin/ivy/learn/api/common/ApiUtils.kt
@@ -1,17 +1,36 @@
 package ivy.learn.api.common
 
+import arrow.core.Either
 import arrow.core.raise.Raise
 import arrow.core.raise.either
 import io.ktor.http.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import ivy.learn.api.common.model.ServerError
 import ivy.learn.api.common.model.ServerErrorResponse
 
+
 @IvyServerDsl
-inline fun <reified T : Any> Routing.getEndpoint(
+inline fun <reified Body : Any, reified Response : Any> Routing.postEndpoint(
     path: String,
-    crossinline handler: suspend Raise<ServerError>.(Parameters) -> T
+    crossinline handler: suspend Raise<ServerError>.(Body, Parameters) -> Response
+) {
+    postEndpointBase(path) { call ->
+        val body = try {
+            call.receive<Body>()
+        } catch (e: Exception) {
+            raise(ServerError.BadRequest("Malformed request body: $e"))
+        }
+        val response = handler(body, call.parameters)
+        call.respond(HttpStatusCode.OK, response)
+    }
+}
+
+@IvyServerDsl
+inline fun <reified Response : Any> Routing.getEndpoint(
+    path: String,
+    crossinline handler: suspend Raise<ServerError>.(Parameters) -> Response
 ) {
     getEndpointBase(path) { call ->
         val response = handler(call.parameters)
@@ -25,11 +44,32 @@ inline fun Routing.getEndpointBase(
     crossinline handler: suspend Raise<ServerError>.(RoutingCall) -> Unit
 ) {
     get(path) {
-        either {
+        handleRequest(handler)
+    }
+}
+
+@IvyServerDsl
+inline fun Routing.postEndpointBase(
+    path: String,
+    crossinline handler: suspend Raise<ServerError>.(RoutingCall) -> Unit
+) {
+    post(path) {
+        handleRequest(handler)
+    }
+}
+
+suspend inline fun RoutingContext.handleRequest(
+    crossinline handler: suspend Raise<ServerError>.(RoutingCall) -> Unit
+) {
+    val result = try {
+        either<ServerError, Unit> {
             handler(call)
-        }.onLeft { error ->
-            respondError(error)
         }
+    } catch (e: Throwable) {
+        Either.Left(ServerError.Unknown("Unexpected error occurred."))
+    }
+    result.onLeft { error ->
+        respondError(error)
     }
 }
 
