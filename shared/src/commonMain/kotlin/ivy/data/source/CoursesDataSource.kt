668d4c95e2da8de53acdef281fbe17ba96b9cc33
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-08 23:20:35 +0200

    Add CoursesDataSource

--- shared/src/commonMain/kotlin/ivy/data/source/CoursesDataSource.kt
+++ shared/src/commonMain/kotlin/ivy/data/source/CoursesDataSource.kt
@@ -1,7 +1,10 @@
 package ivy.data.source
 
+import IvyConstants
 import arrow.core.Either
 import arrow.core.raise.catch
+import arrow.core.raise.either
+import arrow.core.raise.ensure
 import arrow.core.right
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -11,7 +14,9 @@ import ivy.data.ServerUrlProvider
 import ivy.data.headerSessionToken
 import ivy.data.source.model.CourseResponse
 import ivy.model.CourseId
+import ivy.model.LessonId
 import ivy.model.auth.SessionToken
+import ivy.model.course.CourseProgressRequest
 
 class CoursesDataSource(
     private val httpClient: HttpClient,
@@ -29,5 +34,30 @@ class CoursesDataSource(
         }.body<CourseResponse>().right()
     }) { e ->
         Either.Left("Failed to fetch '${courseId.value}' course: ${e.message}")
+    }
+
+    suspend fun saveProgress(
+        session: SessionToken,
+        course: CourseId,
+        lesson: LessonId,
+    ): Either<String, Unit> = catch({
+        either {
+            val response = httpClient.post(
+                "${urlProvider.serverUrl}/lessons/${course.value}/progress"
+            ) {
+                header(IvyConstants.HEADER_SESSION_TOKEN, session.value)
+                contentType(ContentType.Application.Json)
+                setBody(
+                    CourseProgressRequest(
+                        completedLesson = lesson,
+                    )
+                )
+            }
+            ensure(response.status.isSuccess()) {
+                "Failed to save course progress, status - ${response.status}"
+            }
+        }
+    }) { e ->
+        Either.Left("Failed to save course progress: $e")
     }
 }
