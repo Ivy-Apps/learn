90f8153b7ffd6a371459bd201614055c38ffb124
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-22 22:57:50 +0200

    Fix server tests

--- server/src/test/kotlin/ivy/learn/testsupport/data/repository/LessonProgressFixtures.kt
+++ server/src/test/kotlin/ivy/learn/testsupport/data/repository/LessonProgressFixtures.kt
@@ -1,22 +1,22 @@
 package ivy.learn.testsupport.data.repository
 
-import ivy.learn.Lesson.LessonProgressDto
+import ivy.learn.AnswerId
+import ivy.learn.ChoiceOptionId
 import ivy.learn.LessonItemId
-import ivy.model.AnswerId
-import ivy.model.ChoiceOptionId
+import ivy.model.lesson.LessonProgressDto
 
 object LessonProgressFixtures {
-    fun state(
-        selectedAnswers: Map<LessonItemId, Set<AnswerId>> = emptyMap(),
-        openAnswersInput: Map<LessonItemId, String> = emptyMap(),
-        chosen: Map<LessonItemId, ChoiceOptionId> = emptyMap(),
-        answered: Set<LessonItemId> = emptySet(),
-        completed: Set<LessonItemId> = emptySet(),
-    ) = LessonProgressDto(
-        selectedAnswers = selectedAnswers,
-        openAnswersInput = openAnswersInput,
-        chosen = chosen,
-        answered = answered,
-        completed = completed,
-    )
+  fun state(
+    selectedAnswers: Map<LessonItemId, Set<AnswerId>> = emptyMap(),
+    openAnswersInput: Map<LessonItemId, String> = emptyMap(),
+    chosen: Map<LessonItemId, ChoiceOptionId> = emptyMap(),
+    answered: Set<LessonItemId> = emptySet(),
+    completed: Set<LessonItemId> = emptySet(),
+  ) = LessonProgressDto(
+    selectedAnswers = selectedAnswers,
+    openAnswersInput = openAnswersInput,
+    chosen = chosen,
+    answered = answered,
+    completed = completed,
+  )
 }
