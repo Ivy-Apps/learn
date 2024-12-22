c193af13d13242af5e86002aecae42327edcfe19
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-23 01:09:02 +0200

    Fix BE tests

--- server/src/test/kotlin/ivy/learn/api/CoursesApiTest.kt
+++ server/src/test/kotlin/ivy/learn/api/CoursesApiTest.kt
@@ -1,54 +1,57 @@
 package ivy.learn.api
 
 import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.shouldBe
 import ivy.data.source.CoursesDataSource
-import ivy.data.source.model.CourseResponse
 import ivy.di.Di
-import ivy.learn.data.cms.course.programming.ProgrammingFundamentals
+import ivy.learn.data.cms.course.CoursesContent
 import ivy.learn.testsupport.ServerTest
 import org.junit.Test
 
 class CoursesApiTest : ServerTest() {
-    @Test
-    fun `fetches course`() = beTest {
-        // Given
-        val auth = registerUser()
-        val datasource = Di.get<CoursesDataSource>()
+  @Test
+  fun `fetches courses`() = beTest {
+    CoursesContent.courses.forEach { course ->
+      // Given
+      val auth = registerUser()
+      val datasource = Di.get<CoursesDataSource>()
+      val courseId = course.id
 
-        // When
-        val result = datasource.fetchCourseById(
-            session = auth.session.token,
-            courseId = ProgrammingFundamentals.course.id
-        )
+      // When
+      val result = datasource.fetchCourseById(
+        session = auth.session.token,
+        courseId = courseId
+      )
 
-        // Then
-        result.shouldBeRight() shouldBe CourseResponse(
-            course = ProgrammingFundamentals.course,
-            lessons = ProgrammingFundamentals.lessons,
-        )
+      // Then
+      withClue("courseId = ${courseId.value}") {
+        result.shouldBeRight().lessons.shouldNotBeEmpty()
+      }
     }
+  }
 
-    @Test
-    fun `saves course progress`() = beTest {
-        // Given
-        val auth = registerUser()
-        val datasource = Di.get<CoursesDataSource>()
-        val course = ProgrammingFundamentals.course
+  @Test
+  fun `saves course progress`() = beTest {
+    // Given
+    val auth = registerUser()
+    val datasource = Di.get<CoursesDataSource>()
+    val course = CoursesContent.courses.first()
 
-        // When
-        val progressResponse = datasource.saveProgress(
-            session = auth.session.token,
-            course = course.id,
-            lesson = course.lessons.first()
-        )
-        val courseResponse = datasource.fetchCourseById(
-            session = auth.session.token,
-            courseId = course.id,
-        )
+    // When
+    val progressResponse = datasource.saveProgress(
+      session = auth.session.token,
+      course = course.id,
+      lesson = course.lessons.first()
+    )
+    val courseResponse = datasource.fetchCourseById(
+      session = auth.session.token,
+      courseId = course.id,
+    )
 
-        // Then
-        progressResponse.shouldBeRight()
-        courseResponse.shouldBeRight().lessons.first().completed shouldBe true
-    }
+    // Then
+    progressResponse.shouldBeRight()
+    courseResponse.shouldBeRight().lessons.first().completed shouldBe true
+  }
 }
