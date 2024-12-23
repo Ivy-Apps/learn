223c701b11b5b4bbfc74eba8085781cfb6a15167
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-24 01:28:08 +0200

    Update course image

--- server/src/main/kotlin/ivy/learn/data/cms/course/algorithms/AlgorithmFoundations.kt
+++ server/src/main/kotlin/ivy/learn/data/cms/course/algorithms/AlgorithmFoundations.kt
@@ -5,7 +5,7 @@ import ivy.learn.data.cms.dsl.CourseDsl
 object AlgorithmFoundations : CourseDsl({
   name = "Algorithm Foundations"
   tagline = "Demystify space & time complexity, Big O... The first step to becoming an algorithm master."
-  imageUrl = "https://i.ibb.co/566dj34/algo-fundamentals.webp"
+  imageUrl = "https://i.ibb.co/ctdCRzy/algorithm-foundation.webp"
   lesson(
     name = "Time Complexity: What and Why?",
     tagline = "An introduction to Big O and its real-world importance.",
