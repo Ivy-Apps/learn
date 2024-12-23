0e8decb8ab4498b3b2c2e3ea2dc4f72fb7eb26c8
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-24 01:29:14 +0200

    Add ids to lessons

--- server/src/main/kotlin/ivy/learn/data/cms/course/algorithms/AlgorithmFoundations.kt
+++ server/src/main/kotlin/ivy/learn/data/cms/course/algorithms/AlgorithmFoundations.kt
@@ -7,11 +7,13 @@ object AlgorithmFoundations : CourseDsl({
   tagline = "Demystify space & time complexity, Big O... The first step to becoming an algorithm master."
   imageUrl = "https://i.ibb.co/ctdCRzy/algorithm-foundation.webp"
   lesson(
+    id = "time-complexity-what-and-why",
     name = "Time Complexity: What and Why?",
     tagline = "An introduction to Big O and its real-world importance.",
     imageUrl = "https://i.ibb.co/0Qrzpss/time-complexity.webp",
   )
   lesson(
+    id = "time-complexity-types",
     name = "Time Complexity: Types",
     tagline = "O(1), O(log(n)), O(n), O(n * log(n)), O(n^2), O(2^n), O(n!),... and how to find them!?",
     imageUrl = "https://i.ibb.co/hWPHBcW/time-complexity-types.webp",
@@ -22,14 +24,12 @@ object AlgorithmFoundations : CourseDsl({
     tagline = "Understand how memory scales—no fluff, just the essentials",
     imageUrl = "https://i.ibb.co/x6bqy32/space-complexity-banner.webp"
   )
-
   lesson(
     id = "big-o-advanced",
     name = "Big O: Advanced",
     tagline = "Take your algorithm analysis to the next level",
     imageUrl = "https://i.ibb.co/XSJHyyp/big-o-advanced.webp"
   )
-
   lesson(
     id = "space-time-complexity-practice",
     name = "Space-time Complexity: Practice",
