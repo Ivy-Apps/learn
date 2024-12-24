1632b4f0fa929f9cde54dccdd5ec9aff11fb813b
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-24 02:00:00 +0200

    Add linked lists

--- server/src/main/kotlin/ivy/learn/data/cms/course/algorithms/DataStructures.kt
+++ server/src/main/kotlin/ivy/learn/data/cms/course/algorithms/DataStructures.kt
@@ -3,67 +3,70 @@ package ivy.learn.data.cms.course.algorithms
 import ivy.learn.data.cms.dsl.CourseDsl
 
 object DataStructures : CourseDsl({
-    name = "Data Structures"
-    tagline = "Lists, Sets, Maps, Stacks, Trees and more."
-    imageUrl = "https://i.ibb.co/54cv11j/ds-course.webp"
-    lesson(
-        name = "Arrays",
-        tagline = "The simplest data structure.",
-        imageUrl = "https://i.ibb.co/Zxb5XyV/arrays.webp"
-    )
-    lesson(
-        name = "Lists",
-        tagline = "Arrays with superpowers. Dynamic sizing.",
-        imageUrl = "https://i.ibb.co/GnpM6Lb/lists.webp"
-    )
-    lesson(
-        name = "Linked Lists",
-        tagline = "A different take on lists. Pointers and nodes.",
-        imageUrl = "https://i.ibb.co/rHq5PZL/linked-lists.webp"
-    )
-    lesson(
-        name = "Sets",
-        tagline = "Unique elements. No duplicates.",
-        imageUrl = "https://i.ibb.co/HzMFHVV/sets.webp"
-    )
-    lesson(
-        name = "Maps",
-        tagline = "Key-Value pairs. Associative arrays.",
-        imageUrl = "https://i.ibb.co/D4VbPT9/maps.webp"
-    )
-    lesson(
-        name = "Stacks",
-        tagline = "LIFO: Last In, First Out.",
-        imageUrl = "https://i.ibb.co/kc06zjZ/stack.webp"
-    )
-    lesson(
-        name = "Queues",
-        tagline = "FIFO: First In, First Out.",
-        imageUrl = "https://i.ibb.co/6s05L65/queues.webp"
-    )
-    lesson(
-        name = "Trees",
-        tagline = "Hierarchical data structures.",
-        imageUrl = "https://i.ibb.co/kG4wqnv/trees.webp"
-    )
-    lesson(
-        name = "Graphs",
-        tagline = "Networks of nodes and edges.",
-        imageUrl = "https://i.ibb.co/cbsSHcj/graphs.webp"
-    )
-    lesson(
-        name = "Heaps",
-        tagline = "Priority Queues. Efficiently find the max or min.",
-        imageUrl = "https://i.ibb.co/0y7Stzd/heaps.webp"
-    )
-    lesson(
-        name = "Tries",
-        tagline = "Prefix Trees. Efficient string search.",
-        imageUrl = "https://i.ibb.co/4T8XKrc/tries.webp"
-    )
-    lesson(
-        name = "Hash Tables",
-        tagline = "Fast lookups. Key-Value pairs.",
-        imageUrl = "https://i.ibb.co/BKNN73g/hash-tables.webp"
-    )
+  name = "Data Structures"
+  tagline = "Lists, Sets, Maps, Stacks, Trees and more."
+  imageUrl = "https://i.ibb.co/54cv11j/ds-course.webp"
+  lesson(
+    id = "arrays",
+    name = "Arrays",
+    tagline = "The simplest data structure.",
+    imageUrl = "https://i.ibb.co/Zxb5XyV/arrays.webp"
+  )
+  lesson(
+    id = "lists",
+    name = "Lists",
+    tagline = "Arrays with superpowers. Dynamic sizing.",
+    imageUrl = "https://i.ibb.co/GnpM6Lb/lists.webp"
+  )
+  lesson(
+    id = "linked-lists",
+    name = "Linked Lists",
+    tagline = "A different take on lists. Pointers and nodes.",
+    imageUrl = "https://i.ibb.co/rHq5PZL/linked-lists.webp"
+  )
+  lesson(
+    name = "Sets",
+    tagline = "Unique elements. No duplicates.",
+    imageUrl = "https://i.ibb.co/HzMFHVV/sets.webp"
+  )
+  lesson(
+    name = "Maps",
+    tagline = "Key-Value pairs. Associative arrays.",
+    imageUrl = "https://i.ibb.co/D4VbPT9/maps.webp"
+  )
+  lesson(
+    name = "Stacks",
+    tagline = "LIFO: Last In, First Out.",
+    imageUrl = "https://i.ibb.co/kc06zjZ/stack.webp"
+  )
+  lesson(
+    name = "Queues",
+    tagline = "FIFO: First In, First Out.",
+    imageUrl = "https://i.ibb.co/6s05L65/queues.webp"
+  )
+  lesson(
+    name = "Trees",
+    tagline = "Hierarchical data structures.",
+    imageUrl = "https://i.ibb.co/kG4wqnv/trees.webp"
+  )
+  lesson(
+    name = "Graphs",
+    tagline = "Networks of nodes and edges.",
+    imageUrl = "https://i.ibb.co/cbsSHcj/graphs.webp"
+  )
+  lesson(
+    name = "Heaps",
+    tagline = "Priority Queues. Efficiently find the max or min.",
+    imageUrl = "https://i.ibb.co/0y7Stzd/heaps.webp"
+  )
+  lesson(
+    name = "Tries",
+    tagline = "Prefix Trees. Efficient string search.",
+    imageUrl = "https://i.ibb.co/4T8XKrc/tries.webp"
+  )
+  lesson(
+    name = "Hash Tables",
+    tagline = "Fast lookups. Key-Value pairs.",
+    imageUrl = "https://i.ibb.co/BKNN73g/hash-tables.webp"
+  )
 })
