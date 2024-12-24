b3ddb0bf1e44297ad958d0742ebebbedb2655cd4
Iliyan Germanov <iliyan.germanov971@gmail.com>
2024-12-24 18:41:39 +0200

    Remove shuffling

--- composeApp/src/commonMain/kotlin/ui/screen/lesson/mapper/LessonViewStateMapper.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/lesson/mapper/LessonViewStateMapper.kt
@@ -146,7 +146,6 @@ class LessonViewStateMapper(
     clarification = clarification,
     type = if (correct.size == 1) QuestionTypeViewState.SingleChoice else QuestionTypeViewState.MultipleChoice,
     answers = answers.map { it.toViewState(this, localState) }
-      .shuffled()
       .toImmutableList(),
     answered = id in localState.answered,
   )
