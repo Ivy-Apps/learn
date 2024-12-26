bc400b4e6de87805cf9892541b80182232b72e60
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-27 00:15:14 +0200

    Fix KPI

--- server/src/main/kotlin/ivy/learn/domain/analytics/kpi/ActiveUsersAvgCompletedLessonsKpi.kt
+++ server/src/main/kotlin/ivy/learn/domain/analytics/kpi/ActiveUsersAvgCompletedLessonsKpi.kt
@@ -2,23 +2,35 @@ package ivy.learn.domain.analytics.kpi
 
 import ivy.data.source.model.KpiDto
 import ivy.learn.data.database.tables.AnalyticsTable
-import org.jetbrains.exposed.sql.avg
-import org.jetbrains.exposed.sql.countDistinct
+import org.jetbrains.exposed.sql.count
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class ActiveUsersAvgCompletedLessonsKpi : Kpi {
   override suspend fun compute(): KpiDto = transaction {
-    val count = AnalyticsTable
-      .select(AnalyticsTable.userId.countDistinct().avg())
-      .where { AnalyticsTable.event eq "lesson__complete" }
-      .groupBy(AnalyticsTable.userId)
-      .having {
-        AnalyticsTable.userId.countDistinct() greaterEq 1
+    var sum = 0L
+    var count = 0
+    exec(
+      stmt = """
+SELECT COUNT(*) FROM analytics 
+    WHERE event = 'lesson__complete' 
+    GROUP by user_id 
+    HAVING count(*) >= 1        
+      """,
+      args = listOf(
+        AnalyticsTable.userId.columnType to "lesson__complete",
+        AnalyticsTable.userId.count().columnType to 1L,
+      ).take(0),
+      explicitStatementType = null,
+      transform = { rs ->
+        while (rs.next()) {
+          sum += rs.getLong(1)
+          count++
+        }
       }
-      .single()[AnalyticsTable.userId.countDistinct().avg()]
+    )
     KpiDto(
       name = "# of avg completed lessons per active user",
-      text = count?.toLong()?.toString() ?: "null :(",
+      text = (sum / count.toDouble()).toString(),
     )
   }
 }
