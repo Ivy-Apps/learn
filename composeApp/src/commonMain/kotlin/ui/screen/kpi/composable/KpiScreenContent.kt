aa7d2cafe4e990de3180c8c4a3c6c43ee92f4a02
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-27 01:21:38 +0200

    Add LazyColumn key

--- composeApp/src/commonMain/kotlin/ui/screen/kpi/composable/KpiScreenContent.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/kpi/composable/KpiScreenContent.kt
@@ -2,7 +2,7 @@ package ui.screen.kpi.composable
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -48,7 +48,10 @@ private fun Content(
     verticalArrangement = Arrangement.spacedBy(12.dp),
     horizontalAlignment = Alignment.Start,
   ) {
-    items(items = viewState.kpis) { item ->
+    itemsIndexed(
+      items = viewState.kpis,
+      key = { index, item -> "${index}_${item.name}" }
+    ) { _, item ->
       KpiItem(item = item)
     }
   }
