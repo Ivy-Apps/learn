a2f9dcca500c58a3dbb9f50aff66f1e346709602
Nicole <nicole.georgievva@gmail.com>
2024-12-27 18:54:21 +0200

    Fix IvySwitch

--- composeApp/src/commonMain/kotlin/component/button/IvySwitch.kt
+++ composeApp/src/commonMain/kotlin/component/button/IvySwitch.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import ui.theme.IvyTheme
 import ui.theme.colorsExt
@@ -37,13 +38,17 @@ fun IvySwitch(
         color = MaterialTheme.colorsExt.backgroundVariant,
         shape = MaterialTheme.shapes.medium
       )
-      .padding(horizontal = 16.dp),
+      .padding(
+        start = 24.dp,
+        end = 16.dp
+      ),
     verticalAlignment = Alignment.CenterVertically,
     horizontalArrangement = Arrangement.Center
   ) {
     CompositionLocalProvider(
       LocalTextStyle provides IvyTheme.typography.b2.copy(
-        color = MaterialTheme.colorsExt.onBackgroundVariant
+        color = MaterialTheme.colorsExt.onBackgroundVariant,
+        fontWeight = FontWeight.Medium
       )
     ) {
       text()
