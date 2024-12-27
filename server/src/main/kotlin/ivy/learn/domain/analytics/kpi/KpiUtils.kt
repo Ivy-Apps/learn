b80f728edd4ae86e2f32f064a36915eb3091c3a4
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-27 17:17:49 +0200

    Refactor

--- server/src/main/kotlin/ivy/learn/domain/analytics/kpi/KpiUtils.kt
+++ server/src/main/kotlin/ivy/learn/domain/analytics/kpi/KpiUtils.kt
@@ -2,8 +2,10 @@ package ivy.learn.domain.analytics.kpi
 
 import ivy.learn.data.database.tables.AnalyticsTable
 import ivy.learn.data.database.tables.MetricsTable
+import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.countDistinct
+import org.jetbrains.exposed.sql.statements.StatementType
 
 @Suppress("unused")
 fun Transaction.totalDistinctCount(event: String): Long {
@@ -21,3 +23,29 @@ fun Transaction.analyticsDistinctCount(event: String): Long = AnalyticsTable
   .select(AnalyticsTable.userId.countDistinct())
   .where { AnalyticsTable.event eq event }
   .single()[AnalyticsTable.userId.countDistinct()]
+
+@Suppress("unused")
+fun Transaction.analyticsDistinctUserIdCountByParams(
+  eventName: String,
+  onProcess: (params: Map<String, String>, count: Long) -> Unit,
+) {
+  exec(
+    stmt = """
+SELECT params::TEXT, count(DISTINCT user_id) FROM analytics 
+    WHERE event = ? 
+    GROUP by params::TEXT 
+    ORDER BY count(DISTINCT user_id) DESC;       
+      """,
+    args = listOf(
+      AnalyticsTable.event.columnType to eventName,
+    ),
+    explicitStatementType = StatementType.SELECT,
+    transform = { rs ->
+      while (rs.next()) {
+        val params = Json.decodeFromString<Map<String, String>>(rs.getString(1))
+        val count = rs.getLong(2)
+        onProcess(params, count)
+      }
+    }
+  )
+}
--- server/src/main/kotlin/ivy/learn/domain/analytics/kpi/TopItemDistinctUserIdEventCountKpi.kt
+++ server/src/main/kotlin/ivy/learn/domain/analytics/kpi/TopItemDistinctUserIdEventCountKpi.kt
@@ -1,9 +1,6 @@
 package ivy.learn.domain.analytics.kpi
 
 import ivy.data.source.model.KpiDto
-import ivy.learn.data.database.tables.AnalyticsTable
-import kotlinx.serialization.json.Json
-import org.jetbrains.exposed.sql.statements.StatementType
 import org.jetbrains.exposed.sql.transactions.transaction
 
 abstract class TopItemDistinctUserIdEventCountKpi : Kpi {
@@ -13,25 +10,12 @@ abstract class TopItemDistinctUserIdEventCountKpi : Kpi {
 
   override suspend fun compute(): KpiDto = transaction {
     val textBuilder = StringBuilder()
-    exec(
-      stmt = """
-SELECT params::TEXT, count(DISTINCT user_id) FROM analytics 
-    WHERE event = ? 
-    GROUP by params::TEXT 
-    ORDER BY count(DISTINCT user_id) DESC;       
-      """,
-      args = listOf(
-        AnalyticsTable.event.columnType to eventName,
-      ),
-      explicitStatementType = StatementType.SELECT,
-      transform = { rs ->
-        while (rs.next()) {
-          val params = Json.decodeFromString<Map<String, String>>(rs.getString(1))
-          val count = rs.getLong(2)
-          textBuilder.append(
-            "${itemLine(params)}: $count\n"
-          )
-        }
+    analyticsDistinctUserIdCountByParams(
+      eventName = eventName,
+      onProcess = { params, count ->
+        textBuilder.append(
+          "${itemLine(params)}: $count\n"
+        )
       }
     )
     KpiDto(
