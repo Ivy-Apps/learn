d83816b6277c6f055fa9c858a093916fbd92a48e
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-27 16:29:24 +0200

    Fix incorrectly computed KPIs

--- server/src/main/kotlin/ivy/learn/domain/analytics/kpi/TopLessonsByViewsKpi.kt
+++ server/src/main/kotlin/ivy/learn/domain/analytics/kpi/TopLessonsByViewsKpi.kt
@@ -1,40 +1,11 @@
 package ivy.learn.domain.analytics.kpi
 
-import ivy.data.source.model.KpiDto
-import ivy.learn.data.database.tables.AnalyticsTable
 import ivy.model.analytics.AnalyticsParams
-import kotlinx.serialization.json.Json
-import org.jetbrains.exposed.sql.statements.StatementType
-import org.jetbrains.exposed.sql.transactions.transaction
 
-class TopLessonsByViewsKpi : Kpi {
-  override suspend fun compute(): KpiDto = transaction {
-    val textBuilder = StringBuilder()
-
-    exec(
-      stmt = """
-SELECT params::TEXT, count(*) FROM analytics 
-    WHERE event = ? 
-    GROUP by params::TEXT 
-    ORDER BY count(*) DESC;       
-      """,
-      args = listOf(
-        AnalyticsTable.event.columnType to "lesson__view",
-      ),
-      explicitStatementType = StatementType.SELECT,
-      transform = { rs ->
-        while (rs.next()) {
-          val params = Json.decodeFromString<Map<String, String>>(rs.getString(1))
-          val count = rs.getLong(2)
-          textBuilder.append(
-            "${params[AnalyticsParams.courseId]}/${params[AnalyticsParams.lessonId]}: $count\n"
-          )
-        }
-      }
-    )
-    KpiDto(
-      name = "Top Lessons by views",
-      value = textBuilder.toString(),
-    )
+class TopLessonsByViewsKpi : TopItemDistinctUserIdEventCountKpi() {
+  override val metricName = "Top Lessons by distinct user_id views"
+  override val eventName = "lesson__view"
+  override fun itemLine(params: Map<String, String>): String {
+    return "${params[AnalyticsParams.courseId]}/${params[AnalyticsParams.lessonId]}"
   }
 }
